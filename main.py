5b94ec4ac4d20cce1fa999d107c4ff0da713e331
Mohamed Omar <mohamed.omar@brightinsight.com>
2021-09-06 11:58:41 -0700

    Adding the needed Panda's to provide tibular format & providing runtime sorting

--- main.py
+++ main.py
@@ -15,54 +15,111 @@ Tasks:
 - Bonus: Implement the rest call asynchronously
 """
 
+
+
+""""
+Assumptiopns : 
+        - The command line will be passed with correct case sensetive arguments [TODO: enhancement]
+        - The target endpoint of the service is active & accessable
+        - Only implementing the happy path 
+
+"""
+
+
+
+from typing import Dict
 import requests
 import json
 import sys
 import datetime
+import pandas as pd
+from tabulate import tabulate
 
 
-key =""
+#KEY GLOBAL VARAIBLRES 
+KEY =""
+RESPONSE_CODE =""
+SORT_KEY = ""
 headers =  {"Content-Type":"application/json"}
 api_url = "https://ancient-wood-1161.getsandbox.com:443/results"
 #getting response with headers
 
+
+#Function to return the response of Post request using the URL & adds headers to the request
 def get_response(url, header_information):
 
     response = requests.post(api_url, headers=headers)
     data = response.json()
-    response_code = response.status_code
-    return data
+    RESPONSE_CODE  = response.status_code
+    return [data, RESPONSE_CODE]
 
-#resetting syntax to use "" instead of ''
 
+
+#Format the data received yo beautify
 def format_output(data):
     print(json.dumps(data, indent=4, sort_keys=True))
 
-#Traverse
 
-#priniting output
-#print(response_code)
+
+#Main program entry points
 def main():
-    data = get_response(api_url, headers)
-    if len(sys.argv) > 1:
-        key = sys.argv[1]
-        print("***{}***\n".format(key))
-        #.sort(key=lambda d: datetime.strftime(d, "%b %d, %Y %I:%M:%S %p"))
-        data[key].sort(key = lambda i: datetime.datetime.strptime(i["publicationDate"], "%b %d, %Y %I:%M:%S %p"), reverse = True)
 
-        debug(data, key)
+    #Get the data based on the URL & Headeres
+    [data, RESPONSE_CODE] = get_response(api_url, headers)
+
+    #Print the response code that was returned 
+    print("Response Code : {} ".format(RESPONSE_CODE))
+    
+    #Check for passed paramters at run time 
+    count = len(sys.argv)
+    if count == 2:
+        KEY = sys.argv[1]
+        data[KEY].sort(key = lambda i: datetime.datetime.strptime(i["publicationDate"], "%b %d, %Y %I:%M:%S %p"), reverse = True)
+        print_tabular_format_for_sport_type(data[KEY])
+
+
+    elif count == 3:
+         KEY = sys.argv[1]
+         SORT_KEY  = sys.argv[2]
+         data[KEY].sort(key = lambda i: i[SORT_KEY], reverse = True)         
+         print_tabular_format_for_sport_type(data[KEY])
     else:
-        format_output(data)
+
+        for key in data.keys():
+            print("*" * 20)
+            print(key)
+            print("*" * 20)
+            print_tabular_format_for_sport_type(data[key])
+         
 
 
+#Formating the data based on the key with the assumption that the key exists
 def debug(data,key):
     print("=" * 20) 
     print(json.dumps(data[key], indent=4, sort_keys=True))
     print("=" * 20)
     print("\n")
 
-#execute the module
 
+def print_tabular_format_for_sport_type(dataset):
+    keys = []
+    vals = []
+    for data in dataset:
+        val = []
+        for k,v in data.items():
+            keys.append(k)
+            val.append(v)
+        vals.append(val)
+
+    df = pd.DataFrame([v for v in vals], columns=list(dict.fromkeys(keys)))
+    print(tabulate(df, headers='keys', tablefmt='psql'))
+
+def print_type(sports_data):
+    if type(sports_data) is dict:
+        for i in sports_data.keys():
+            print ("Results Type: {}".format(i))
+
+#execute the module
 if __name__ == "__main__":
     main()
 
